5bbe678fa32c4cb9fb856d7947554a7dfcb7f095
Pankajsiwach123321 <pankusiwach000@gmai.com>
2023-10-17 11:03:17 +0530

    add hover on nav logo

--- assets/js/mfs.js
+++ assets/js/mfs.js
@@ -1,25 +1,19 @@
 let nav = document.querySelector(".nav");
 let menubar = document.querySelector(".menubar");
+let loadtake = document.querySelector(".nav-logo")
 let finish = document.querySelectorAll(".finish");
 let overlay = document.querySelector(".over-layer")
 let bgbody = document.querySelector(".bg-bodys");
-let fix1 = document.querySelector(".fix1");
-let fix2 = document.querySelector(".fix2");
-let fix3 = document.querySelector(".fix3");
 nav.addEventListener("click", function () {
+    loadtake.classList.toggle("nav-logo-routate")
     menubar.classList.toggle("left-0")
-    fix1.classList.toggle("fixer1")
-    fix2.classList.toggle("fixer2")
-    fix3.classList.toggle("fixer3")
     overlay.classList.toggle("left-0")
     bgbody.classList.toggle("over-flow")
     // nav.classList.toggle("left-corner")
 })
 overlay.addEventListener("click", function () {
     menubar.classList.toggle("left-0")
-    fix1.classList.toggle("fixer1")
-    fix2.classList.toggle("fixer2")
-    fix3.classList.toggle("fixer3")
+    loadtake.classList.toggle("nav-logo-routate")
     overlay.classList.toggle("left-0")
     bgbody.classList.toggle("over-flow")
     // nav.classList.toggle("left-corner")
@@ -28,8 +22,8 @@ finish.forEach(e => {
     e.addEventListener("click", function () {
         menubar.classList.remove("left-0")
         bgbody.classList.remove("over-flow")
-        fix1.classList.toggle("fixer1")
         overlay.classList.toggle("left-0")
+        loadtake.classList.toggle("nav-logo-routate")
         fix2.classList.toggle("fixer2")
         fix3.classList.toggle("fixer3")
         // nav.classList.toggle("left-corner")
