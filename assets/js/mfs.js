087e83c9cf5f6aa4eb17af02225ef13043392df9
Pankajsiwach123321 <pankusiwach000@gmai.com>
2023-10-20 13:47:17 +0530

    updates

--- assets/js/mfs.js
+++ assets/js/mfs.js
@@ -4,15 +4,23 @@ let loadtake = document.querySelector(".nav-logo")
 let finish = document.querySelectorAll(".finish");
 let overlay = document.querySelector(".over-layer")
 let bgbody = document.querySelector(".bg-bodys");
+let fix1 = document.querySelector(".fix1");
+let fix2 = document.querySelector(".fix2");
+let fix3 = document.querySelector(".fix3");
 nav.addEventListener("click", function () {
-    loadtake.classList.toggle("nav-logo-routate")
     menubar.classList.toggle("left-0")
+    fix1.classList.toggle("fixer1")
+    fix2.classList.toggle("fixer2")
+    fix3.classList.toggle("fixer3")
     overlay.classList.toggle("left-0")
     bgbody.classList.toggle("over-flow")
     // nav.classList.toggle("left-corner")
 })
 overlay.addEventListener("click", function () {
     menubar.classList.toggle("left-0")
+    fix1.classList.toggle("fixer1")
+    fix2.classList.toggle("fixer2")
+    fix3.classList.toggle("fixer3")
     loadtake.classList.toggle("nav-logo-routate")
     overlay.classList.toggle("left-0")
     bgbody.classList.toggle("over-flow")
@@ -21,6 +29,9 @@ overlay.addEventListener("click", function () {
 finish.forEach(e => {
     e.addEventListener("click", function () {
         menubar.classList.remove("left-0")
+        fix1.classList.toggle("fixer1")
+        fix2.classList.toggle("fixer2")
+        fix3.classList.toggle("fixer3")
         bgbody.classList.remove("over-flow")
         overlay.classList.toggle("left-0")
         loadtake.classList.toggle("nav-logo-routate")
